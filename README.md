05d3a6cbee1954cd301cf50a55b40f5e8c6231b3
Kelsey Hrubes <kelseyhrubes@gmail.com>
2018-04-01 17:21:28 -0700

    work on request

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Home automation tools
 # ip address
 192.168.86.182
 
+# python wrapper 
+https://github.com/MySportsFeeds/mysportsfeeds-python
+
 # sports apis
 ## Only pro sports - https://www.mysportsfeeds.com/data-feeds/api-docs/#
 - Scoreboard https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/scoreboard.json?fordate=20171024
--- src/Test.py
+++ src/Test.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 import flux_led
+import requests
+import base64
 
-light = flux_led.WifiLedBulb("192.168.86.182")
-light.turnOn()
+# light = flux_led.WifiLedBulb("192.168.86.182")
+# light.turnOff()
 
+print("test")
+
+url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/scoreboard.json?fordate=20180401'
+headers = {'encrypted_credentials': base64.b64encode('kelseyhrubes:blakeiscute')}
+response = requests.get(url, headers=headers)
+
+
+print(response)
